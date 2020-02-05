8875895cfd8136461cbd2b625f28b047147dc67c
Hussain Alaidarous <hussaino3@hotmail.com>
2020-02-05 16:22:05 +0700

    simplified user informtion acquisition

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -4,20 +4,20 @@ const { ApolloServer } = require('apollo-server-lambda');
 
 // Construct a schema, using GraphQL schema language
 const server = new ApolloServer({
-    typeDefs: schema,
-    resolvers,
-    // context: async ({ event }) => {
-    //     console.log('getting user');
-    //     const user = await getPayload(event.headers.Authorization);
-    //     return {
-    //         user,
-    //     };
-    // },
+	typeDefs: schema,
+	resolvers,
+	context: ({ event }) => {
+		console.log('getting user');
+		const user = event.requestContext.authorizer.claims;
+		return {
+			user,
+		};
+	},
 });
 
 exports.handler = server.createHandler({
-    cors: {
-        origin: true,
-        credentials: true,
-    },
+	cors: {
+		origin: true,
+		credentials: true,
+	},
 });
